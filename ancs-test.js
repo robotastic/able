8283424940cc0713f34623530ddabb4ce21764b5
robotastic <luke@robotastic.com>
2015-09-27 22:13:19 -0400

    f

--- ancs-test.js
+++ ancs-test.js
@@ -177,7 +177,7 @@ able.on('accept', function(peripheral) {
     });
 */
     able.findHandlesForUuid(peripheral.id, SERVICE_UUID);
-  /* ancs.discoverServicesAndCharacteristics(function() {
+   ancs.discoverServicesAndCharacteristics(function() {
 
         var handle = able._bindings._handles[ancs.uuid];
          var aclStream = able._bindings._aclStreams[handle];
@@ -194,7 +194,7 @@ able.on('accept', function(peripheral) {
 
     });
 
-*/
+
 
 
 /*
